ca3c64bcb87839c61c703fbcc74393a1a1d8a0c7
Abbasa5251 <abbasa5251@hotmail.com>
2024-08-20 00:11:17 +0530

    feat: added feedback creation time to table

--- app/(user)/projects/[projectId]/page.tsx
+++ app/(user)/projects/[projectId]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { format } from "date-fns";
+import { enUS } from "date-fns/locale";
 import { useQuery } from "convex/react";
 import React from "react";
 import {
@@ -11,6 +13,12 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { StarIcon } from "lucide-react";
 
@@ -37,6 +45,7 @@ function ProjectPage({ params: { projectId } }: Props) {
 						<TableHead>Name</TableHead>
 						<TableHead>Email</TableHead>
 						<TableHead>Message</TableHead>
+						<TableHead>Created at</TableHead>
 						<TableHead className="text-right">Rating</TableHead>
 					</TableRow>
 				</TableHeader>
@@ -49,6 +58,32 @@ function ProjectPage({ params: { projectId } }: Props) {
 								</TableCell>
 								<TableCell>{f.email}</TableCell>
 								<TableCell>{f.content}</TableCell>
+								<TableCell>
+									<TooltipProvider>
+										<Tooltip>
+											<TooltipTrigger>
+												{format(
+													f._creationTime,
+													"do MMM yyyy",
+													{
+														locale: enUS,
+													}
+												)}
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>
+													{format(
+														f._creationTime,
+														"do MMM yyyy, h:mm a",
+														{
+															locale: enUS,
+														}
+													)}
+												</p>
+											</TooltipContent>
+										</Tooltip>
+									</TooltipProvider>
+								</TableCell>
 								<TableCell className="flex items-center justify-end">
 									{[...Array(5)].map((_, idx) => (
 										<StarIcon
